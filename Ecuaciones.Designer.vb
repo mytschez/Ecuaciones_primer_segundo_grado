1c70d4f3cbc76070c3e208ea30b046465467e87b
mytschez <72097976+mytschez@users.noreply.github.com>
2021-02-23 19:34:46 +0100

    Add files via upload

---
+++ Ecuaciones.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEcuaciones
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.rbPrimerGrado = New System.Windows.Forms.RadioButton()
+        Me.rbSegundoGrado = New System.Windows.Forms.RadioButton()
+        Me.lblTipoEcuacion = New System.Windows.Forms.Label()
+        Me.PanelElegir = New System.Windows.Forms.Panel()
+        Me.lblsubtitulo = New System.Windows.Forms.Label()
+        Me.lblFormula = New System.Windows.Forms.Label()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.lblX2 = New System.Windows.Forms.Label()
+        Me.lblCero = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.lblX = New System.Windows.Forms.Label()
+        Me.PanelEcPrimGrado = New System.Windows.Forms.Panel()
+        Me.PanelElegir.SuspendLayout()
+        Me.PanelEcPrimGrado.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTitulo.Location = New System.Drawing.Point(23, 32)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(641, 60)
+        Me.lblTitulo.TabIndex = 88
+        Me.lblTitulo.Text = "Método para resolver la incógnita de una ecuación de primer grado."
+        '
+        'rbPrimerGrado
+        '
+        Me.rbPrimerGrado.AutoSize = True
+        Me.rbPrimerGrado.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.rbPrimerGrado.Font = New System.Drawing.Font("Century Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.rbPrimerGrado.Location = New System.Drawing.Point(21, 30)
+        Me.rbPrimerGrado.Name = "rbPrimerGrado"
+        Me.rbPrimerGrado.Size = New System.Drawing.Size(176, 29)
+        Me.rbPrimerGrado.TabIndex = 89
+        Me.rbPrimerGrado.Text = "Primer grado"
+        Me.rbPrimerGrado.UseVisualStyleBackColor = True
+        '
+        'rbSegundoGrado
+        '
+        Me.rbSegundoGrado.AutoSize = True
+        Me.rbSegundoGrado.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.rbSegundoGrado.Font = New System.Drawing.Font("Century Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.rbSegundoGrado.Location = New System.Drawing.Point(21, 66)
+        Me.rbSegundoGrado.Name = "rbSegundoGrado"
+        Me.rbSegundoGrado.Size = New System.Drawing.Size(204, 29)
+        Me.rbSegundoGrado.TabIndex = 90
+        Me.rbSegundoGrado.Text = "Segundo grado"
+        Me.rbSegundoGrado.UseVisualStyleBackColor = True
+        '
+        'lblTipoEcuacion
+        '
+        Me.lblTipoEcuacion.AutoSize = True
+        Me.lblTipoEcuacion.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblTipoEcuacion.Location = New System.Drawing.Point(796, 20)
+        Me.lblTipoEcuacion.Name = "lblTipoEcuacion"
+        Me.lblTipoEcuacion.Size = New System.Drawing.Size(180, 23)
+        Me.lblTipoEcuacion.TabIndex = 3
+        Me.lblTipoEcuacion.Text = "Tipo de ecuación"
+        '
+        'PanelElegir
+        '
+        Me.PanelElegir.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PanelElegir.Controls.Add(Me.rbSegundoGrado)
+        Me.PanelElegir.Controls.Add(Me.rbPrimerGrado)
+        Me.PanelElegir.Location = New System.Drawing.Point(786, 31)
+        Me.PanelElegir.Name = "PanelElegir"
+        Me.PanelElegir.Size = New System.Drawing.Size(245, 109)
+        Me.PanelElegir.TabIndex = 4
+        '
+        'lblsubtitulo
+        '
+        Me.lblsubtitulo.AutoSize = True
+        Me.lblsubtitulo.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblsubtitulo.Location = New System.Drawing.Point(23, 118)
+        Me.lblsubtitulo.Name = "lblsubtitulo"
+        Me.lblsubtitulo.Size = New System.Drawing.Size(418, 28)
+        Me.lblsubtitulo.TabIndex = 87
+        Me.lblsubtitulo.Text = "Introduce los coeficientes 'A' y 'B' : "
+        '
+        'lblFormula
+        '
+        Me.lblFormula.AutoSize = True
+        Me.lblFormula.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblFormula.Location = New System.Drawing.Point(23, 162)
+        Me.lblFormula.Name = "lblFormula"
+        Me.lblFormula.Size = New System.Drawing.Size(126, 28)
+        Me.lblFormula.TabIndex = 86
+        Me.lblFormula.Text = "Ax + B = 0"
+        '
+        'txtA
+        '
+        Me.txtA.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtA.Location = New System.Drawing.Point(23, 239)
+        Me.txtA.MaxLength = 5
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(90, 57)
+        Me.txtA.TabIndex = 0
+        Me.txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtB
+        '
+        Me.txtB.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtB.Location = New System.Drawing.Point(13, 14)
+        Me.txtB.MaxLength = 5
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(90, 57)
+        Me.txtB.TabIndex = 1
+        Me.txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblX2
+        '
+        Me.lblX2.AutoSize = True
+        Me.lblX2.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblX2.Location = New System.Drawing.Point(119, 249)
+        Me.lblX2.Name = "lblX2"
+        Me.lblX2.Size = New System.Drawing.Size(123, 47)
+        Me.lblX2.TabIndex = 10
+        Me.lblX2.Text = "x^2 +"
+        '
+        'lblCero
+        '
+        Me.lblCero.AutoSize = True
+        Me.lblCero.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblCero.Location = New System.Drawing.Point(288, 24)
+        Me.lblCero.Name = "lblCero"
+        Me.lblCero.Size = New System.Drawing.Size(77, 47)
+        Me.lblCero.TabIndex = 11
+        Me.lblCero.Text = "= 0"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCalcular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSeaGreen
+        Me.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCalcular.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnCalcular.Location = New System.Drawing.Point(786, 253)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(245, 73)
+        Me.btnCalcular.TabIndex = 3
+        Me.btnCalcular.Text = "&Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.IndianRed
+        Me.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSalir.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnSalir.Location = New System.Drawing.Point(786, 383)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(245, 73)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "&Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'lblResultado
+        '
+        Me.lblResultado.Font = New System.Drawing.Font("Century Gothic", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblResultado.Location = New System.Drawing.Point(23, 341)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(590, 145)
+        Me.lblResultado.TabIndex = 15
+        '
+        'txtC
+        '
+        Me.txtC.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtC.Location = New System.Drawing.Point(192, 14)
+        Me.txtC.MaxLength = 5
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(90, 57)
+        Me.txtC.TabIndex = 2
+        Me.txtC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblX
+        '
+        Me.lblX.AutoSize = True
+        Me.lblX.Font = New System.Drawing.Font("Century Gothic", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblX.Location = New System.Drawing.Point(109, 24)
+        Me.lblX.Name = "lblX"
+        Me.lblX.Size = New System.Drawing.Size(77, 47)
+        Me.lblX.TabIndex = 17
+        Me.lblX.Text = "x +"
+        '
+        'PanelEcPrimGrado
+        '
+        Me.PanelEcPrimGrado.Controls.Add(Me.lblCero)
+        Me.PanelEcPrimGrado.Controls.Add(Me.lblX)
+        Me.PanelEcPrimGrado.Controls.Add(Me.txtB)
+        Me.PanelEcPrimGrado.Controls.Add(Me.txtC)
+        Me.PanelEcPrimGrado.Location = New System.Drawing.Point(263, 226)
+        Me.PanelEcPrimGrado.Name = "PanelEcPrimGrado"
+        Me.PanelEcPrimGrado.Size = New System.Drawing.Size(382, 81)
+        Me.PanelEcPrimGrado.TabIndex = 1
+        '
+        'frmEcuaciones
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1065, 495)
+        Me.Controls.Add(Me.PanelEcPrimGrado)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.lblX2)
+        Me.Controls.Add(Me.txtA)
+        Me.Controls.Add(Me.lblFormula)
+        Me.Controls.Add(Me.lblsubtitulo)
+        Me.Controls.Add(Me.lblTipoEcuacion)
+        Me.Controls.Add(Me.PanelElegir)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmEcuaciones"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ecuaciones"
+        Me.PanelElegir.ResumeLayout(False)
+        Me.PanelElegir.PerformLayout()
+        Me.PanelEcPrimGrado.ResumeLayout(False)
+        Me.PanelEcPrimGrado.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents rbPrimerGrado As RadioButton
+    Friend WithEvents rbSegundoGrado As RadioButton
+    Friend WithEvents lblTipoEcuacion As Label
+    Friend WithEvents PanelElegir As Panel
+    Friend WithEvents lblsubtitulo As Label
+    Friend WithEvents lblFormula As Label
+    Friend WithEvents txtA As TextBox
+    Friend WithEvents txtB As TextBox
+    Friend WithEvents lblX2 As Label
+    Friend WithEvents lblCero As Label
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents txtC As TextBox
+    Friend WithEvents lblX As Label
+    Friend WithEvents PanelEcPrimGrado As Panel
+End Class
---
+++ Ecuaciones.vb
@@ -0,0 +1,174 @@
+﻿Public Class frmEcuaciones
+    Private Sub frmEcuaciones_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'al cargar el formulario marca la opción de ecuación de primer grado
+        rbPrimerGrado.Checked = True
+        txtB.Focus()
+    End Sub
+    'radiobuttons
+    Private Sub rbPrimerGrado_CheckedChanged(sender As Object, e As EventArgs) Handles rbPrimerGrado.CheckedChanged
+        CheckRadioButton()
+    End Sub
+
+    Private Sub rbSegundoGrado_CheckedChanged(sender As Object, e As EventArgs) Handles rbSegundoGrado.CheckedChanged
+        CheckRadioButton()
+    End Sub
+
+    'click en el botón calcular
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        CalcularEcuaciones()
+    End Sub
+
+    'al hacer click en el botón salir se cierra el formualrio
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Application.Exit()
+    End Sub
+
+
+#Region "procedimientos"
+
+    Private Sub CalcularEcuaciones()
+        'variables
+        Dim A As Single
+        Dim B As Single
+        Dim C As Single
+        'si el valor introducido contiene "." como separador decimal muestra un mensaje
+        If txtA.Text.Contains(".") Or txtB.Text.Contains(".") Or txtC.Text.Contains(".") Then
+            lblResultado.Text = "El separador decimal debe ser la 'coma', no el 'punto'"
+        Else
+            Try
+                If rbPrimerGrado.Checked Then
+                    'comprobar que los coeficientes introducidos están en el formato correcto
+                    A = Single.Parse(txtB.Text)
+                    B = Single.Parse(txtC.Text)
+                    'procedimiento que calcula la ecuación de primer grado
+                    PrimerGrado(A, B)
+                Else
+                    'comprobar que los coeficientes introducidos están en el formato correcto
+                    A = Single.Parse(txtA.Text)
+                    B = Single.Parse(txtB.Text)
+                    C = Single.Parse(txtC.Text)
+                    'si el parámetro A de la ecuación de segundo grado es cero, 
+                    'resuleve la ecuación como si fuese de primer grado
+                    If A = 0 Then
+                        PrimerGrado(B, C)
+                    Else
+                        SegundoGrado(A, B, C)
+                    End If
+                End If
+            Catch ex As Exception
+                lblResultado.Text = "Los argumentos nos son válidos."
+                'vaciar los cuadros de texto
+                txtA.Clear()
+                txtB.Clear()
+                txtC.Clear()
+            End Try
+        End If
+    End Sub
+
+    'ecuaciones de primer grado
+    Sub PrimerGrado(a As Single, b As Single)
+        'variable
+        Dim incog As Single
+        'si el coeficiente A es distinto de cero calcula la ecuación de primer grado
+        If a <> 0 Then
+            incog = -b / a
+            'si el resultado es -0 muestra 0
+            If incog = -0 Then
+                lblResultado.Text = "La solución de la ecuación es: " & vbCrLf & vbCrLf & "x = " & Math.Round(-incog, 4)
+            Else
+                lblResultado.Text = "La solución de la ecuación es: " & vbCrLf & vbCrLf & "x = " & Math.Round(incog, 4)
+            End If
+
+        Else
+            lblResultado.Text = "El parámetro A no puede ser 0."
+        End If
+    End Sub
+
+    'ecuaciones de segundo grado
+    Sub SegundoGrado(a As Single, b As Single, c As Single)
+        'variables
+        Dim incog1 As Single
+        Dim incog2 As Single
+        Dim raiz As Single
+        'raiz, operación que está dentro de la raíz cuadrada
+        raiz = b ^ 2 - (4 * a * c)
+
+        If raiz > 0 Then
+            ' es la raiz de un número positivo y mayor que cero
+            'calcular los dos resultados posibles
+            incog1 = (-b + Math.Sqrt(raiz)) / 2 * a
+            incog2 = (-b - Math.Sqrt(raiz)) / 2 * a
+
+            lblResultado.Text = "La solución de la ecuación es:" & vbCrLf & vbCrLf &
+            "x1 = " & Math.Round(incog1, 4) & vbCrLf &
+            "x2 = " & Math.Round(incog2, 4)
+
+        ElseIf raiz = 0 Then
+            'si es la raiz de cero el resultado es único
+            incog1 = -b / 2 * a
+
+            lblResultado.Text = "La solución de la ecuación es :" & vbCrLf & vbCrLf &
+            "x = " & Math.Round(incog1, 4)
+        Else
+            lblResultado.Text = "La ecuación no tiene soluciones reales."
+        End If
+
+    End Sub
+
+    'procedimiento para adaptar el formulario a ambos tipos de ecuaciones
+    Sub CheckRadioButton()
+        'variable
+        Dim posicion As Point
+
+        If rbPrimerGrado.Checked Then
+            'focus en el cuadro de texto del primer coeficiente
+            txtB.Focus()
+
+            'coordenadas del panel - ecuación de primer grado
+            posicion.X = 23
+            posicion.Y = 229
+            PanelEcPrimGrado.Location = posicion
+
+            'cambiar el encabezado del formulario para ecuación de primer grado
+            lblTitulo.Text = "Método para resolver la incógnita de una ecuación de primer grado."
+            lblsubtitulo.Text = "Introduce los coeficientes 'A' y 'B'  : "
+            lblFormula.Text = "Ax + B = 0"
+
+            'ocultar las partes que sólo son para las ecuaciones de segundo grado
+            txtA.Hide()
+            lblX2.Hide()
+
+            'limpiar resultado
+            txtA.Clear()
+            txtB.Clear()
+            txtC.Clear()
+            lblResultado.Text = ""
+        Else
+            'focus en el cuadro de texto del primer coeficiente
+            txtA.Focus()
+
+            'coordenadas del panel - ecuación de primer grado
+            posicion.X = 230
+            posicion.Y = 229
+            PanelEcPrimGrado.Location = posicion
+
+            'cambiar el encabezado del formulario para ecuación de segundo grado
+            lblTitulo.Text = "Método para resolver las incógnitas de una ecuación de segundo grado."
+            lblsubtitulo.Text = "Introduce los coeficientes 'A', 'B' y 'C' : "
+            lblFormula.Text = "Ax^2 + Bx + C = 0"
+
+            'mostrar las partes que sólo son para las ecuaciones de segundo grado
+            txtA.Show()
+            lblX2.Show()
+
+            'limpiar resultado y cuadros de texto
+            txtA.Clear()
+            txtB.Clear()
+            txtC.Clear()
+            lblResultado.Text = ""
+        End If
+    End Sub
+#End Region
+
+
+End Class
